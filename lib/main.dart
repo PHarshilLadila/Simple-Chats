8671b27aae151a260f1bc8bdb63249080b52b9ce
PHarshilLadila <harshilgajipara.ladila@gmail.com>
2025-05-13 11:37:25 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+/// This is the main application widget.
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 Future<void> main() async {
